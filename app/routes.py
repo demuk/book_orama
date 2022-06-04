e59dde3d00f8d27a370f535cbaedbf2336d0ed09
kiptoo <denysmutay@gmail.com>
2022-06-05 01:03:56 +0300

    added view function for register and login in

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,14 @@ from app import app
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
